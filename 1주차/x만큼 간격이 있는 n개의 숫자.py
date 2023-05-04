7d02899e9c043e430019121b10278a66f57a102d
UsoD98 <130049758+UsoD98@users.noreply.github.com>
2023-05-04 14:54:35 +0900

    Delete 1주차 directory

--- 1주차/x만큼 간격이 있는 n개의 숫자.py
+++
@@ -1,7 +0,0 @@
-def solution(x, n):
-    answer = []
-    none = 0
-    for i in range(n):
-        none += x
-        answer.append(none)
-    return answer
--- 1주차/자릿수 더하기.py
+++
@@ -1,7 +0,0 @@
-def solution(n):
-    answer = 0
-    str_n = str(n)
-    for i in str_n:
-        answer += int(i)
-
-    return answer
