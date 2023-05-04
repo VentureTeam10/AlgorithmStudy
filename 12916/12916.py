d65cda3d266d04c36286824f1448b66c01161a2c
UsoD98 <130049758+UsoD98@users.noreply.github.com>
2023-05-04 14:49:30 +0900

    Create 12916.py

---
+++ 12916/12916.py
@@ -0,0 +1,13 @@
+#문자열 내 p와 y의 개수
+def solution(s):
+    p_num = 0
+    y_num = 0
+    s = s.lower()
+    answer = True
+    for i in range(len(s)):
+        if s[i] == 'p':
+            p_num += 1
+        elif s[i] == 'y':
+            y_num += 1
+    answer = p_num == y_num
+    return answer
