21f58bc84395c80c72d4564849d43fde3588da0a
UsoD98 <130049758+UsoD98@users.noreply.github.com>
2023-05-04 14:51:39 +0900

    Create 42576.py

---
+++ 42576/42576.py
@@ -0,0 +1,24 @@
+#완주하지 못한 선수
+def solution(participant, completion):
+    answer = ''
+    dic_completion = {}
+    dic_participant = {}
+
+    for c in completion:
+        if dic_completion.get(c) != None:
+            dic_completion[c] += 1
+        else:
+            dic_completion[c] = 1
+
+    for p in participant:
+        if p in dic_participant:
+            dic_participant[p] += 1
+        else:
+            dic_participant[p] = 1
+            
+        if p not in dic_completion:
+            answer = p
+        elif p in dic_completion and dic_completion[p] < dic_participant[p]:
+            answer = p
+        
+    return answer
